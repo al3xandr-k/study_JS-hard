135856650723759fb72dcf8ae7a0d1ece7ab8e0d
Alexander Kupsha <red2@mail.ru>
2021-02-03 16:52:58 +0200

    Исправил задание LessonHard04

--- js/main.js
+++ js/main.js
@@ -2,17 +2,17 @@
 
 const fnc = function (str) {
   console.log(typeof str);
-  if (typeof str !== 'string') {
-    console.log('Это не строка');
-  } else if(typeof str === 'string') {
+  if (str !== String(str)) {
+    console.log('Это не строка.');
+  } else if(str === String(str)) {
     console.log(String('Это строка.').replace(/\s+/g, ''));
   };
 
   if (str.length > 30){
     console.log(String(str).substr(0, 30) + '...'); 
   } else {
-    console.log('а эта строка меньше чем 30 символов.');
-  }
+    console.log('А эта строка меньше чем 30 символов.');
+  };
 };
 //'Строка содержит больше чем 30 символов.'
 fnc(1);
