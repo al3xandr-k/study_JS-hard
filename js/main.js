5b0a40a1015f5c13dbbb3f9e246ce54caab4eb86
Alexander Kupsha <red2@mail.ru>
2021-02-03 14:45:16 +0200

    Finished LessonHard04

--- js/main.js
+++ js/main.js
@@ -1,41 +1,36 @@
 'use strict'
 
-//1 ЗАДАНИЕ
-let lang = 'en';
-
-let arr = [
-  ['Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье'],
-  ['Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday']
-];
+let string1 = 'Это не строка.';
+let string2 = 'Это строка.';
+let string3 = 'Текст состоящий из 30 символов.';
 
 
-if (lang === 'ru') {
-  console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
-} else if (lang === 'en') {
-  console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
-}
+//Условие 1
 
-switch (lang) {
-  case 'ru':
-    console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
-    break;
-  case 'en':
-    console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
-    break;
-  default:
-    console.log('что та пошло не так');
+function foo(param) {
+  if (param !== String(param)){
+    console.log(string1);
+  } else if (param === String(param)){
+    console.log(string2);
+  }
+};
+foo(1);
+
+//Условие 2
+
+function foo2(param) {
+  if (param !== String(param)){
+    console.log(string1);
+  } else if (param === String(param)){
+    console.log(string2.replace(/\s+/g, ''));
+  }
 };
 
-for (let i = 0; i < arr.length - 1; i++) {
-  lang === 'ru' ? console.log(arr[0].toString()) :
-    lang === 'en' ? console.log(arr[1].toString()) :
-      console.log('что та пошло не так');
+foo2('1');
+
+//Условие 3
+function foo3() {
+  console.log(string3 = string3.substr(0, 30) + '...');
 };
 
-
-// 2 ЗАДАНИЕ
-let namePerson = 'Максим';
-
-(namePerson === 'Максим') ? console.log('Директор') :
-  (namePerson === 'Максим') ? console.log('Преподаватель') :
-    console.log('Студент');
+foo3();
