4b9be8f98cb34a2ad30cfdb2556e68f8973856c0
Alexander Kupsha <red2@mail.ru>
2021-02-03 16:44:07 +0200

    исправил задание Lesson04Hard

--- js/main.js
+++ js/main.js
@@ -1,36 +1,18 @@
-'use strict'
+'use strict';
 
-let string1 = 'Это не строка.';
-let string2 = 'Это строка.';
-let string3 = 'Текст состоящий из 30 символов.';
+const fnc = function (str) {
+  console.log(typeof str);
+  if (typeof str !== 'string') {
+    console.log('Это не строка');
+  } else if(typeof str === 'string') {
+    console.log(String('Это строка.').replace(/\s+/g, ''));
+  };
 
-
-//Условие 1
-
-function foo(param) {
-  if (param !== String(param)){
-    console.log(string1);
-  } else if (param === String(param)){
-    console.log(string2);
+  if (str.length > 30){
+    console.log(String(str).substr(0, 30) + '...'); 
+  } else {
+    console.log('а эта строка меньше чем 30 символов.');
   }
 };
-foo(1);
-
-//Условие 2
-
-function foo2(param) {
-  if (param !== String(param)){
-    console.log(string1);
-  } else if (param === String(param)){
-    console.log(string2.replace(/\s+/g, ''));
-  }
-};
-
-foo2('1');
-
-//Условие 3
-function foo3() {
-  console.log(string3 = string3.substr(0, 30) + '...');
-};
-
-foo3();
+//'Строка содержит больше чем 30 символов.'
+fnc(1);
