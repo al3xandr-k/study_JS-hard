8709b434b0a0e8132ad4f95b925eac5a4012bfcf
Alexander Kupsha <red2@mail.ru>
2021-02-11 21:53:19 +0200

    Finished task Hard LessonHard07

--- js/main.js
+++ js/main.js
@@ -7,13 +7,13 @@ const outText3 = document.querySelector('.out3');
 const outText4 = document.querySelector('.out4');
 let currentDate = new Date();
 
+//1
 outText1.innerHTML = week;
-
+//2
 for (let i = 0; i < week.length; i++) {
   outText2.innerHTML += week[i] + '<br>';
 }
-
+//3
 outText3.innerHTML = `${week.splice(0, 5)},<strong>${week.splice(-2)}</strong>`;
-
-
+//4
 outText4.innerHTML = `<strong>${currentDate.toString().split(' ').slice(0, 3).join(' ')}</strong>`;
