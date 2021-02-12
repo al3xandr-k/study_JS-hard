e74ec1e2d10ca24d4c1d40fe1bfa323a3b02c6ad
Alexander Kupsha <red2@mail.ru>
2021-02-12 12:06:15 +0200

    rewrote task

--- js/main.js
+++ js/main.js
@@ -2,6 +2,6 @@
 
 const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
 const outText = document.querySelector('.out');
-let currentDate = new Date().toString().split(' ').slice(0, 3).join(' ');
+let currentDate = (new Date()).toString().split(' ').slice(0, 3).join(' ');
 
 outText.innerHTML = `${week[0]}<br>${week[1]}<br>${week[2]}<br>${week[3]}<br><strong>${week[4]} - ${currentDate}</strong><br><i>${week[5]}</i><br><i>${week[6]}</i><br>`
