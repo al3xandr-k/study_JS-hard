2d08b61d1c7e083218dfed8fea76dc5ee1ec1e5f
Alexander Kupsha <red2@mail.ru>
2021-02-12 12:05:39 +0200

    rewrote task

--- js/main.js
+++ js/main.js
@@ -1,19 +1,7 @@
 'use strict'
 
 const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
-const outText1 = document.querySelector('.out1');
-const outText2 = document.querySelector('.out2');
-const outText3 = document.querySelector('.out3');
-const outText4 = document.querySelector('.out4');
-let currentDate = new Date();
+const outText = document.querySelector('.out');
+let currentDate = new Date().toString().split(' ').slice(0, 3).join(' ');
 
-//1
-outText1.innerHTML = week;
-//2
-for (let i = 0; i < week.length; i++) {
-  outText2.innerHTML += week[i] + '<br>';
-}
-//3
-outText3.innerHTML = `${week.splice(0, 5)},<strong>${week.splice(-2)}</strong>`;
-//4
-outText4.innerHTML = `<strong>${currentDate.toString().split(' ').slice(0, 3).join(' ')}</strong>`;
+outText.innerHTML = `${week[0]}<br>${week[1]}<br>${week[2]}<br>${week[3]}<br><strong>${week[4]} - ${currentDate}</strong><br><i>${week[5]}</i><br><i>${week[6]}</i><br>`
