4c04ef694946c6cce3cd40b004ad721a148eca04
Alexander Kupsha <red2@mail.ru>
2021-02-13 13:53:47 +0200

    lessonHard07

--- js/main.js
+++ js/main.js
@@ -2,6 +2,11 @@
 
 const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
 const outText = document.querySelector('.out');
-let currentDate = (new Date()).toString().split(' ').slice(0, 3).join(' ');
 
-outText.innerHTML = `${week[0]}<br>${week[1]}<br>${week[2]}<br>${week[3]}<br><strong>${week[4]} - ${currentDate}</strong><br><i>${week[5]}</i><br><i>${week[6]}</i><br>`
+for (let i = 0; i < week.length; i++) {
+  outText.innerHTML += week[i] + '<br>';
+}
+
+
+
+// let currentDate = (new Date()).toString().split(' ').slice(0, 3).join(' ');
