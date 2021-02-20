d04da57efe00dfd2833f303abcc111477244e7eb
Alexander Kupsha <red2@mail.ru>
2021-02-20 16:10:47 +0200

    in progress

--- js/main.js
+++ js/main.js
@@ -8,8 +8,26 @@
 
 const day = new Date();
 const outText = document.querySelector('.out');
-outText.innerHTML = day; 
 
 const formateDate = (date) => {
+  let getDate = date.getDate();
+  if (getDate < 10) getDate = '0' + getDate;
+
+  let getMonth = date.getMonth() + 1;
+  if (getMonth < 10) getMonth = '0' + getMonth;
+
+  let fullYear = date.getFullYear() % 100;
+  if (fullYear < 10) fullYear = '0' + fullYear;
+
+  return getDate + '.' + getMonth + '.' + fullYear;
 
 };
+
+function checkTime(time) {
+  if (time < 10) {
+    time = "0" + time;
+  }
+  return time;
+}
+
+outText.innerHTML = formateDate(day) + ' - ' + checkTime(day.getHours()) + '.' + checkTime(day.getMinutes()) + '.' + checkTime(day.getSeconds());
