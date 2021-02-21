f1f64ef447c2d4c88e358dff1ecab81e5cebb52d
Alexander Kupsha <red2@mail.ru>
2021-02-21 14:54:45 +0200

    Finished task

--- js/main.js
+++ js/main.js
@@ -1,34 +1,31 @@
 'use strict'
-// 1) Выведите на страницу текущую дату и время в 2-х форматах: 
-//     a) 'Сегодня Вторник, 4 февраля 2020 года, 21 час 5 минут 33 секунды'  
 
-// 2) Для вывода в формате (а) напишите функцию, которая будет менять склонение слов в зависимости от числа, "час, часов, часа"
-// 4) С помощью функции setInterval, реализуйте обновление даты и времени каждую секунду 
+setInterval(() => {
+  const date = new Date();
+  const outText = document.querySelector('.out');
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
 
-const day = new Date();
-const outText = document.querySelector('.out');
+  const formateDate = date => {
+    let getDate = date.getDate();
+    if (getDate < 10) getDate = '0' + getDate;
 
-const formateDate = date => {
-  let getDate = date.getDate();
-  if (getDate < 10) getDate = '0' + getDate;
+    let getMonth = date.getMonth() + 1;
+    if (getMonth < 10) getMonth = '0' + getMonth;
 
-  let getMonth = date.getMonth() + 1;
-  if (getMonth < 10) getMonth = '0' + getMonth;
+    let fullYear = date.getFullYear() % 100;
+    if (fullYear < 10) fullYear = '0' + fullYear;
 
-  let fullYear = date.getFullYear() % 100;
-  if (fullYear < 10) fullYear = '0' + fullYear;
+    return getDate + '.' + getMonth + '.' + fullYear;
+  };
 
-  return getDate + '.' + getMonth + '.' + fullYear;
-
-};
-
-const checkTime = time => {
-  if (time < 10) {
-    time = "0" + time;
+  const checkTime = time => {
+    if (time < 10) {
+      time = "0" + time;
+    }
+    return time;
   }
-  return time;
-}
 
-outText.innerHTML = formateDate(day) + ' - ' + checkTime(day.getHours()) + ':' + checkTime(day.getMinutes()) + ':' + checkTime(day.getSeconds());
-
-//outText.innerHTML =day.toLocaleDateString()+ ' - '+ day.toLocaleTimeString();
+  outText.innerHTML = formateDate(date) + ' - ' + checkTime(hours) + ':' + checkTime(minutes) + ':' + checkTime(seconds);
+}), (1000);
