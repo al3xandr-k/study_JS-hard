ee06420ffe14187ed13ae0323cd25979da851823
Alexander Kupsha <red2@mail.ru>
2021-02-25 14:34:43 +0200

    LessonHard13

--- js/main.js
+++ js/main.js
@@ -1,42 +1,20 @@
-'use strict'
+const btn = document.querySelector('.btn');
 
-const week = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
-const outText = document.querySelector('.out');
-const date = new Date();
-const currentDay = date.getDay();
 
-week.map((day, indexDay) => {
-  if (indexDay > 0 && indexDay < 6) {
-    if (indexDay === currentDay) {
-      const span = document.createElement('span');
-      const br = document.createElement('br');
-      span.innerHTML = day;
-      span.style.fontStyle = 'italic';
-      outText.append(span);
-      outText.append(br);
-    } else {
-      const span = document.createElement('span');
-      const br = document.createElement('br');
-      span.innerHTML = day;
-      outText.append(span);
-      outText.append(br);
-    }
-  } else if (indexDay === 0 || indexDay === 6) {
-    if (indexDay === currentDay) {
-      const span = document.createElement('span');
-      const br = document.createElement('br');
-      span.innerHTML = day;
-      span.style.fontWeight = 'bold';
-      span.style.fontStyle = 'italic';
-      outText.append(span);
-      outText.append(br);
-    } else {
-      const span = document.createElement('span');
-      const br = document.createElement('br');
-      span.innerHTML = day;
-      span.style.fontWeight = 'bold';
-      outText.append(span);
-      outText.append(br);
-    }
-  }
-});
+btn.addEventListener('click', () => {
+  const body = document.querySelector('body');
+
+  let r;
+  let g;
+  let b;
+  let color;
+
+  r = Math.floor(Math.random() * (256));
+  g = Math.floor(Math.random() * (256));
+  b = Math.floor(Math.random() * (256));
+
+  color = `#${r.toString(16)}${g.toString(16)}${b.toString(16)}`;
+  body.style.backgroundColor = color;
+
+  btn.innerHTML = color;
+})
