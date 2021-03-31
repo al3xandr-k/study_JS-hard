6b780684223f333a900635e26354626694b75c6a
Alexander Kupsha <red2@mail.ru>
2021-03-31 18:38:32 +0300

    promise finished

--- script.js
+++ script.js
@@ -1,49 +1,38 @@
 document.addEventListener('DOMContentLoaded', () => {
     'use strict';
 
-    const select = document.getElementById('cars'),
-        output = document.getElementById('output');
+    const select = document.getElementById('cars');
+    const output = document.getElementById('output');
 
     select.addEventListener('change', () => {
         const getData = (url) => {
             return new Promise((resolve, reject) => {
+
                 const request = new XMLHttpRequest();
+
                 request.open('GET', url);
                 request.setRequestHeader('Content-type', 'application/json');
 
                 request.addEventListener('readystatechange', () => {
-                    if (request.readyState === 4) {
-                        return;
-                    };
-
-                    if (request.status === 200) {
+                    if (request.readyState === 4 && request.status === 200) {
                         const data = JSON.parse(request.responseText);
-                        resolve(data);
+                        data.cars.forEach(item => {
+                            if (item.brand === select.value) {
+                                const { brand, model, price } = item;
+
+                                output.innerHTML = `
+                  brand: ${brand}, model: ${model} <br>
+                  price: ${price}
+                  `;
+                            };
+                        });
                     } else {
-                        reject(request.statusText);
+                        output.innerHTML = 'Error';
                     };
                 });
                 request.send();
             });
         };
-
-        const outputCars = (data) => {
-            console.log('data: ', data);
-            data.cars.forEach(item => {
-                console.log('item: ', item);
-                if (item.brand === select.value) {
-                    const { brand, model, price } = item;
-
-                    output.insertAdjacentHTML('beforebegin', `Тачка ${brand} ${model} <br>
-            Цена: ${price}$`);
-                }
-            });
-
-            const cars = getData('cars.json');
-
-            Promise.race(cars)
-                .then(outputCars)
-                .catch(error => console.error(error));
-        }
+        getData('cars.json');
     });
 });
