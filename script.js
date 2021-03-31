f8fc98ad207f10a7862d73390bcb04dfc209e8db
Alexander Kupsha <red2@mail.ru>
2021-03-31 18:56:28 +0300

    promise

--- script.js
+++ script.js
@@ -5,43 +5,36 @@ document.addEventListener('DOMContentLoaded', () => {
     const output = document.getElementById('output');
 
     select.addEventListener('change', () => {
-        const getData = (url) => {
+        const getData = (data) => {
             return new Promise((resolve, reject) => {
-
                 const request = new XMLHttpRequest();
 
-                request.open('GET', url);
+                request.open('GET', data);
+
                 request.setRequestHeader('Content-type', 'application/json');
 
                 request.addEventListener('readystatechange', () => {
                     if (request.readyState === 4 && request.status === 200) {
                         const data = JSON.parse(request.responseText);
                         resolve(data);
+
+                        data.cars.forEach(item => {
+                            if (item.brand === select.value) {
+                                const { brand, model, price } = item;
+
+                                output.innerHTML = `
+                  brand: ${brand}, model: ${model} <br>
+                  price: ${price}
+                  `;
+                            };
+                        });
                     } else {
                         reject(request.statusText);
                     };
                 });
                 request.send();
             });
-        };
-
-        const outputCars = (data) => {
-            data.cars.forEach(item => {
-                if (item.brand === select.value) {
-                    const { brand, model, price } = item;
-
-                    output.insertAdjacentHTML('beforebegin', `
-                    brand: ${brand}, model: ${model} <br>
-                    price: ${price}`
-                    );
-                };
-            });
-        };
-
-        const carsData = getData('cars.json');
-
-        Promise.race(carsData)
-            .then(outputCars)
-            .catch(error => console.error(error));
+        }
+        getData('cars.json');
     });
 });
