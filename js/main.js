126f3a55f42b5bf767d103d6af48c466955e373f
Alexander Kupsha <red2@mail.ru>
2021-04-07 15:45:52 +0300

    finished

--- js/main.js
+++ js/main.js
@@ -1,20 +1,54 @@
-const btn = document.querySelector('.btn');
+const body = document.querySelector('body');
 
+class DomElement {
 
-btn.addEventListener('click', function() {
-  const body = document.querySelector('body');
+  constructor(selector, height, width, bg) {
+    this.selector = selector;
+    this.height = height;
+    this.width = width;
+    this.bg = bg;
+  }
 
-  let r;
-  let g;
-  let b;
-  let color;
+  addElement(selector, height, width, bg) {
 
-  r = Math.floor(Math.random() * (256));
-  g = Math.floor(Math.random() * (256));
-  b = Math.floor(Math.random() * (256));
+    let addDivElement = document.createElement('div');
 
-  color = `#${r.toString(16)}${g.toString(16)}${b.toString(16)}`;
-  body.style.backgroundColor = color;
+    addDivElement.style.position = 'absolute';
+    addDivElement.style.height = height;
+    addDivElement.style.width = width;
+    addDivElement.style.background = bg;
 
-  this.innerHTML = color;
-})
+    document.addEventListener('keydown', (event) => {
+
+      let cs = window.getComputedStyle(addDivElement);
+      let left = parseInt(cs.marginLeft);
+      let top = parseInt(cs.marginTop);
+
+      switch (event.key) {
+        case 'ArrowLeft':
+          if (left > 0) addDivElement.style.marginLeft = left - 10 + 'px';
+          break;
+        case 'ArrowUp':
+          if (top > 0) addDivElement.style.marginTop = top - 10 + 'px';
+          break;
+        case 'ArrowRight':
+          if (left < document.documentElement.clientWidth - 100)
+            addDivElement.style.marginLeft = left + 10 + 'px';
+          break;
+        case 'ArrowDown':
+          if (top < document.documentElement.clientHeight - 100)
+            addDivElement.style.marginTop = top + 10 + 'px';
+          break;
+      }
+
+      console.log('event.key: ', event.key);
+      console.log('event: ', event);
+    });
+
+    selector.append(addDivElement);
+  };
+};
+
+const domElement = new DomElement();
+
+domElement.addElement(body, '100px', '100px', 'red');
